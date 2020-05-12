d30312fb872606a832845ba49571a3ba03ad4b57
Sergio Pinho <sergiopinho@sergios-mini.lan>
2020-05-12 02:07:09 -0400

    createtaroles plugin fully functional

--- src/app/plugins/createtaroles.plugin.ts
+++ src/app/plugins/createtaroles.plugin.ts
@@ -1,6 +1,6 @@
 import { Plugin } from '../../common/plugin';
 import { IContainer, IMessage, ChannelType } from '../../common/types';
-import { GuildChannel } from 'discord.js';
+import { GuildChannel, TextChannel} from 'discord.js';
 
 export class CreateTaRoles extends Plugin {
   public name: string = 'Creat Class TA Role';
@@ -13,6 +13,54 @@ export class CreateTaRoles extends Plugin {
   }
 
   public async execute(message: IMessage, args?: string[]) {
-    message.reply('yes')
+    let channelFromCategoryCS = this._getChannelsFromCategory(message, 'CS-CLASSES')
+    for (let i = 0; i < channelFromCategoryCS.length; i++) {
+      const element = channelFromCategoryCS[i];
+      this._createRole(message,element.name);
+    }
+    let channelFromCategoryIT = this._getChannelsFromCategory(message, 'IT-CLASSES')
+    for (let i = 0; i < channelFromCategoryIT.length; i++) {
+      const element = channelFromCategoryIT[i];
+      this._createRole(message,element.name);
+    }
+
+    message.reply('TA Roles Created for CS and IT Classes')
+  }
+
+  private _getChannelsFromCategory(message: IMessage, categoryName: string) {
+    let categoryChannels: TextChannel[] = []
+
+    const category = message.guild.channels.filter(channel => channel.type === "category");
+
+    let categoryChildren = category.map((channel:any) => {
+      if (channel.name.toLowerCase() === categoryName.toLowerCase()) {
+        return channel.children
+      }
+    })
+
+    categoryChildren.forEach(element => {
+      if (element) {
+        for (const key of element.keys()) {
+          categoryChannels.push(element.get(key))
+        }
+      }
+    });
+    return categoryChannels
+
+  }
+
+  private _createRole(message: IMessage, roleName: string) {
+    let roles = message.guild.roles.filter( role => role.name === roleName)
+    if(roles.size === 0){
+      message.guild.createRole({
+        name: roleName,
+        hoist: true,
+        mentionable: false,
+      });
+      return 1
+    }else{
+      return 0
+    }
+
   }
 }
